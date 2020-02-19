50749bd2d679b263181acf9e62da957ee2d72ee2
MGamal1986 <elsayedmg@gmail.com>
2020-02-19 13:58:55 -0800

    upload bascis files

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector('.btn-expand');
+const lCon = document.querySelector('.left-content');
+const RCon = document.querySelector('.right-content');
+const menu = document.querySelector('.nav-menu');
+const menuItem = document.querySelectorAll('.menu-item');
+const mainContent = document.querySelector('.main-content');
+const btnIcon = document.querySelector('.btn-icon');
+const links = document.querySelectorAll('.menu-link');
+const icons = document.querySelectorAll('.icons i');
+// add event handler
+btn.addEventListener('click',toggleClass);
+
+function toggleClass(){
+  lCon.classList.toggle('show');
+  RCon.classList.toggle('show');
+  btnIcon.classList.toggle('close');
+  btn.classList.toggle('close');
+  menu.classList.toggle("show");
+  links.forEach((link)=>{
+    link.classList.toggle('show');
+  });
+  menuItem.forEach(function(item){
+    item.classList.toggle('show');
+  });
+}
